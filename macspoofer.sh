0d9fb8da83c4710ace24fe69a6f90e5436b02eb6
dimoto95 <tsioumas@protonmail.com>
2017-12-11 00:33:09 +0200

    Fixes #1

--- macspoofer.sh
+++ macspoofer.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "$EUID" != "0" ];
+then
+	echo "This script must be run as root" 1>&2
+	exit
+fi
+
 #Get ethernet interface
 get_interface()
 {
@@ -13,7 +19,7 @@ ORIGINALMAC=`ip addr show ${INTERFACE} | awk 'NR==2{print $2}' `
 echo 'Original Mac =  ' ${ORIGINALMAC}
 VENDORBYTES=`echo ${ORIGINALMAC}| cut -f 1-3 -d ":" `
 NONVENDORBYTES=`echo ${ORIGINALMAC} | cut -f 4-6 -d ":" | rev `
-NEWMAC=`echo ${VENDORBYTES}:${NONVENDORBYTES}`
+NEWMAC="${VENDORBYTES}:${NONVENDORBYTES}"
 echo 'New Mac =	' ${NEWMAC}
 }
 
@@ -35,6 +41,7 @@ else
 	echo "Failed"
 fi 
 }
+
 get_interface
 change_originalmac
 set_newmac
